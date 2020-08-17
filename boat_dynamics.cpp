7c5221195200c9385c38ce108f9cea1e3d165e74
Jack Capper <65087566+jckapper@users.noreply.github.com>
2020-08-17 16:09:15 -0700

    Velocity User Input
    Initial test of user input for initial velocity

--- boat_dynamics.cpp
+++ boat_dynamics.cpp
@@ -26,8 +26,15 @@ namespace boat_dynamics {
         T_0_boatNED_ = Xformd((Vector3d() << 0.0, 0.0, boat_height_m_).finished(), Quatd::from_euler(M_PI, 0.0, 0.0));
         T_NED_0_ = Xformd((Vector3d() << 0.0, 0.0, 0.0).finished(), Quatd::from_euler(M_PI, 0.0, 0.0)).inverse();
         
+        cout << "Enter an initial X velocity: ";
+        cin >> X_velocity;
+        cout << "Enter an initial Y velocity: ";
+        cin >> Y_velocity;
+        cout << "Enter an initial Z velocity: ";
+        cin >> Z_velocity;
+        
         Current_State_.X = (T_0_boat_);
-        Current_State_.v = Vector3d(.05, 0., 0.);
+        Current_State_.v = Vector3d(X_velocity, Y_velocity, Z_Velocity);       
         Current_State_.w = Vector3d(0., 0., 0.);
         Current_State_.q = Quatd::Identity();
 
