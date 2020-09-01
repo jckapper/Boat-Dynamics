dbf9defc96a795cb465384d0007c8d479f583617
Jack Capper <65087566+jckapper@users.noreply.github.com>
2020-09-01 16:58:34 -0400

    Testing Simulation

--- boat_dynamics.cpp
+++ boat_dynamics.cpp
@@ -38,8 +38,8 @@ namespace boat_dynamics {
         Current_State_.w = Vector3d(0., 0., 0.);
         Current_State_.q = Quatd::Identity();
         
-        u_.F = Vector3d(600., 20., 9.81*boat_mass_kg_);//-grav_ * boat_mass_kg_;
-        u_.T = Vector3d(0., 0., 14500.);
+        u_.F = Vector3d(0.0., 0.0., 9.81*boat_mass_kg_);//-grav_ * boat_mass_kg_;
+        u_.T = Vector3d(0., 0., 20500.);
         
         truth_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("boat_truth_NED", 1);
         marker_pub_ = nh_.advertise<visualization_msgs::Marker>("boat_marker", 1);
